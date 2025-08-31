acc16426352e564c208a4bb041527699cccb476b
kadirkhan02 <kadirkhanamt@gmail.com>
2025-08-31 21:57:31 +0530

    first Commit

--- src/main/java/LibraryManagmentSystem/controller/librarianC.java
+++ src/main/java/LibraryManagmentSystem/controller/librarianC.java
@@ -3,36 +3,40 @@ package LibraryManagmentSystem.controller;
 import LibraryManagmentSystem.model.Book;
 import LibraryManagmentSystem.service.librarianS;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("librarian")
 public class librarianC {
 
     @Autowired
     librarianS service;
 
-    @PostMapping("librarian")
-    public void addBook(@RequestBody Book book){
+    @PostMapping
+    public ResponseEntity<String> addBook(@RequestBody Book book){
         service.addBook(book);
+        return ResponseEntity.ok("Successfully Posted");
     }
-    @GetMapping("librarian")
+    @GetMapping
     public List<Book> getAllBook()
     {
         return service.getAllBook();
     }
-    @DeleteMapping("librarian")
+    @DeleteMapping
     public void deleteBook(@RequestBody Book book)
     {
          service.deleteBook(book);
     }
-    @PutMapping("librarian")
+    @PutMapping
     public void updateBook(@RequestBody Book book)
     {
         service.updateBook(book);
     }
-    @GetMapping("librarian/{bookId}")
+    @GetMapping("/{bookId}")
     public Book searchbyId(@PathVariable int bookId)
     {
         return service.searchbyId(bookId);
